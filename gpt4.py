4639012946f10924fc65f079c5af355045cd7c12
gaaran <42284498+MagitekZed@users.noreply.github.com>
2023-07-22 22:57:41 -0700

    Create gpt4.py

---
+++ gpt4.py
@@ -0,0 +1,19 @@
+import os
+import openai
+
+# Load your API key from an environment variable or secret management service
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def generate_response(user_message, user_preferences):
+    # Create a chat completion
+    chat_completion = openai.ChatCompletion.create(
+        model="gpt-4",  # Use the GPT-4 model
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant that provides book recommendations. When asked for book recommendations, you always respond with three books. You will include the Title, the Author, and a brief spoiler-free synopsis of the book. You will format your response to be read via a message on the Telegram app."},
+            {"role": "user", "content": user_message}
+        ]
+    )
+    # Get the model's response
+    response = chat_completion['choices'][0]['message']['content']
+    return response
+
