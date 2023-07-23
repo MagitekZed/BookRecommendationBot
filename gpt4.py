956d2779c0ceba783ba5e14cf580bb9d679e75aa
gaaran <42284498+MagitekZed@users.noreply.github.com>
2023-07-23 15:07:52 -0700

    Update gpt4.py

--- gpt4.py
+++ gpt4.py
@@ -11,7 +11,7 @@ def generate_response(user_message, user_preferences):
         chat_completion = openai.ChatCompletion.create(
             model="gpt-4",  # Use the GPT-4 model
             messages=[
-                {"role": "system", "content": "You are a helpful assistant that provides book recommendations. When asked for book recommendations, you always respond with three books. You will include the Title, the Author, and a brief spoiler-free synopsis of the book. You will format your response to be read via a message on the Telegram app."},
+                {"role": "system", "content": "You are a helpful assistant that provides book recommendations. When asked for book recommendations, you always respond with three books. You will include the Title, the Author, and a detailed but spoiler-free (very important) synopsis of the book. Make sure that the book recommendations are real books, and if the user supplied data is not enough to generate a request, you will provide a friendly error message asking them to please try again and the reason why. You will format your response to be read via a message on the Telegram app."},
                 {"role": "user", "content": user_message}
             ],
             temperature=0.75,
