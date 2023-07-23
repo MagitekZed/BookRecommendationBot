e91914bd5559c595f803d54d02389bfae0055fff
gaaran <42284498+MagitekZed@users.noreply.github.com>
2023-07-22 22:57:13 -0700

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,4 +1,5 @@
 from telebot import types  # Import the types module
+from gpt4 import generate_response
 
 # Global dictionary to store user preferences
 user_data = {}
@@ -8,11 +9,11 @@ def register_handlers(bot):
     @bot.callback_query_handler(func=lambda call: True)
     def callback_query(call):
         # Check the callback_data and call the appropriate function
-        if call.data in ['standalone', 'series', 'nopreference']:
+        if call.data in ['stand-alone', 'series', 'no preference']:
             process_format_step(call.message, call.data)
-        elif call.data in ['newreleases', 'classics', 'nopreference']:
+        elif call.data in ['new releases', 'classics', 'no preference']:
             process_publicationtime_step(call.message, call.data)
-        elif call.data in ['fiction', 'nonfiction']:
+        elif call.data in ['fiction', 'non-fiction']:
             process_type_step(call.message, call.data)
         elif call.data in ['short', 'long']:
             process_length_step(call.message, call.data)
@@ -50,6 +51,23 @@ def register_handlers(bot):
             # If the user hasn't set any preferences yet, send a message letting them know
             bot.send_message(message.chat.id, 'You haven\'t set any reading preferences yet. You can do so by typing /preferences.')
 
+    # This handler is called when the user sends the /get_recommendation command
+    @bot.message_handler(commands=['get_recommendation'])
+    def get_recommendation(message):
+        # Check if the user has set any preferences yet
+        if message.chat.id in user_data:
+            # Retrieve the user's preferences
+            preferences = user_data[message.chat.id]
+            # Generate a message to send to the GPT-4 model based on the user's preferences
+            message_content = f"I'm looking for a {preferences['genre']} book. I prefer {preferences['format']} books and I'm interested in {preferences['time']} books. I like {preferences['type']} books. Some of my favorite authors are {preferences['authors']}, and I recently enjoyed reading {preferences['books']}. I prefer {preferences['length']} books."
+            # Generate a response using the GPT-4 model
+            response = generate_response(message_content, preferences)
+            # Send the response back to the user
+            bot.send_message(message.chat.id, response)
+        else:
+            # If the user hasn't set any preferences yet, send a message letting them know
+            bot.send_message(message.chat.id, 'You haven\'t set any reading preferences yet. You can do so by typing /preferences.')
+
     # This function is called after the user responds to the genre question
     def process_genre_step(message):
         # Store the user's favorite genre
@@ -57,11 +75,11 @@ def register_handlers(bot):
         # Create a new InlineKeyboardMarkup object
         keyboard = types.InlineKeyboardMarkup()
         # Add "Stand-Alone", "Series", and "No Preference" buttons to the keyboard
-        keyboard.add(types.InlineKeyboardButton('Stand-Alone', callback_data='standalone'),
+        keyboard.add(types.InlineKeyboardButton('Stand-Alone', callback_data='stand-alone'),
                      types.InlineKeyboardButton('Series', callback_data='series'),
-                     types.InlineKeyboardButton('No Preference', callback_data='nopreference'))
-        # Ask the user if they prefer standalone books or series, and display the keyboard
-        bot.send_message(message.chat.id, 'Do you prefer standalone books or series?', reply_markup=keyboard)
+                     types.InlineKeyboardButton('No Preference', callback_data='no preference'))
+        # Ask the user if they prefer stand-alone books or series, and display the keyboard
+        bot.send_message(message.chat.id, 'Do you prefer stand-alone books or series?', reply_markup=keyboard)
 
     def process_format_step(message, format):
         # Store the user's format preference
@@ -69,20 +87,20 @@ def register_handlers(bot):
         # Create a new InlineKeyboardMarkup object
         keyboard = types.InlineKeyboardMarkup()
         # Add "New Releases", "Classics", and "No Preference" buttons to the keyboard
-        keyboard.add(types.InlineKeyboardButton('New Releases', callback_data='newreleases'),
+        keyboard.add(types.InlineKeyboardButton('New Releases', callback_data='new releases'),
                      types.InlineKeyboardButton('Classics', callback_data='classics'),
-                     types.InlineKeyboardButton('No Preference', callback_data='nopreference'))
+                     types.InlineKeyboardButton('No Preference', callback_data='no preference'))
         # Ask the user if they prefer new releases or classics, and display the keyboard
         bot.send_message(message.chat.id, 'Do you prefer new releases or classics?', reply_markup=keyboard)
 
     def process_publicationtime_step(message, time):
         # Store the user's time preference
-        user_data[message.chat.id]['Publication Time'] = time
+        user_data[message.chat.id]['Time'] = time
         # Create a new InlineKeyboardMarkup object
         keyboard = types.InlineKeyboardMarkup()
         # Add "Fiction", "Non-Fiction" buttons to the keyboard
         keyboard.add(types.InlineKeyboardButton('Fiction', callback_data='fiction'),
-                     types.InlineKeyboardButton('Non-Fiction', callback_data='nonfiction'))
+                     types.InlineKeyboardButton('Non-Fiction', callback_data='non-fiction'))
         # Ask the user if they're looking for fiction or non-fiction recommendations, and display the keyboard
         bot.send_message(message.chat.id, 'Are you looking for fiction or non-fiction recommendations?', reply_markup=keyboard)
 
